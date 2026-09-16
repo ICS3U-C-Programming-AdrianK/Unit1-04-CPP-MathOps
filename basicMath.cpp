63993ecc5a4237ae05bd5cc83a3e3368fda59248
Adrian kouakam <adrian.kouakam-guemiebing@stu.ocsb.ca>
2026-09-16 20:10:19 +0000

    First commit basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2026 Adrian Student All rights reserved.
+// .
+// Created by: Adrian student
+// Date: sep 16th, 2026
+// This program displays various math equations .
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "2+3=" <<2+3 << std::endl;  // addition
+    std::cout << "9-4=" <<9-4 << std::endl;  // subtraction
+    std::cout << "7*5=" <<7*5 << std::endl;  // multiplication
+    std::cout << "4/2=" <<4/2 << std::endl;  // integer division
+    std::cout << "7/2=" <<7.0/2 << std::endl;  // decimal division
+    std::cout << "5%2=" <<5%2 << std::endl;  // modulo
+    std::cout << "9^2=" <<pow(9 , 2) << std::endl;  // exponent
+    std::cout << "√64=" <<sqrt(64) << std::endl;  // square root
+}
